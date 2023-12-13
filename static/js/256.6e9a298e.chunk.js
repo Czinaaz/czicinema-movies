e83c6c39c3d65a8724f2543535eac744087a6810
Czinaaz <130914798+Czinaaz@users.noreply.github.com>
2023-12-13 14:21:02 +0000

    Deploying to gh-pages from @ Czinaaz/goit-react-hw-05-movies@e9c817598ece0890151da467ef76fa389c3bdffd 🚀

---
+++ static/js/256.6e9a298e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[256],{256:function(e,t,o){o.r(t);var i=o(87),n=o(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsxs)("h3",{children:["404 Page not found, ",(0,n.jsx)(i.rU,{to:"/goit-react-hw-05-movies/",children:"Go to Home Page"})]})})}}}]);
+//# sourceMappingURL=256.6e9a298e.chunk.js.map
